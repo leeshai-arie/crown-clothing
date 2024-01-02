0e48754971357caee0ad4c3a8757e9a0ebf31ded
Leeshai-Arie <leeshaiarie@gmail.com>
2024-01-02 10:27:35 +0200

    update the nav

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-import { App } from "./App.1";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="home" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
