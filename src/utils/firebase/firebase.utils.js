1a28a82f487dc0e9b277111be3edba557abbd395
leeshai-arie <leeshaiarie@gmail.com>
2024-01-16 15:29:35 +0200

    update signinForm

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -24,14 +24,18 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 // Set google provider
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+
 // Set custom parmaters
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 // Initialize FireStore
 const db = getFirestore();
@@ -39,8 +43,21 @@ const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
 
-  console.log(userDocRef);
-
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  return userDocRef;
 };
